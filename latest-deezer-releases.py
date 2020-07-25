62d548a461237d848e562ead74ff43f3a2ed3140
Théo Dumont <theo.dumont@mines-paristech.fr>
2020-07-25 15:41:59 +0200

    Change to-do list to table in order to have more info on releases.

--- latest-deezer-releases.py
+++ latest-deezer-releases.py
@@ -5,24 +5,45 @@ import deezer
 import argparse
 from notion.client import NotionClient
 from notion.block import TodoBlock, DividerBlock, ImageBlock
-from datetime import datetime, timedelta
-from auth import USER_ID, TOKEN_V2, PAGE_URL
+from notion.collection import NotionDate
+from datetime import datetime, timedelta, date, timezone
+from auth import USER_ID, TOKEN_V2, COLLECTION_URL
 
 parser = argparse.ArgumentParser(description="Fetch the latest releases from your favorite artists")
-parser.add_argument("-d", "--nb-days", default=7, help="how far you want to go in days", type=int)
+parser.add_argument("-d", "--nb-days", default=2, help="how far you want to go (days)", type=int)
+parser.add_argument("-u", "--utc-offset", default=2, help="offset with UTC time zone (hours)", type=int)
+parser.add_argument("-r", "--remove-checked", action="store_true", help="remove checked elements even if they are recent")
 args = parser.parse_args()
 
-nb_days_delta = args.nb_days
 
-
-## UTILS ##
-
-def format_release(album):
-    """Prettily display album in Notion."""
-    return f"__{album.artist.name}__ - [{album.title}]({album.link})"
+nb_days = args.nb_days
+nb_days_delta = timedelta(days=nb_days)
+utc_offset = args.utc_offset
+utc_offset_delta = timedelta(hours=utc_offset)
+remove_all_checked = args.remove_checked
 
 
 ## DEEZER ##
+
+def make_dict(client, album):
+    album_dict = {}
+    # basic info
+    album_dict["artist"] = album.artist.name
+    album_dict["title"] = album.title
+    album_dict["type"] = album.record_type
+    # length of tracklist
+    album_dict["tracks"] = len(album.get_tracks())
+    # 1st genre of album
+    album_genre = client.get_genre(album.genre_id).name
+    album_dict["genre"] = album_genre if album_genre != -1 else None
+    # url
+    album_dict["url"] = album.link
+    # release date
+    release_datetime = datetime.strptime(album.release_date, '%Y-%m-%d')
+    release_date = release_datetime.date()  # to get rid of release time
+    album_dict["released_on"] = NotionDate(start=release_date)
+
+    return album_dict
 
 def get_artist_from_album_search(client, artist_request_name):
     """Get artist using an album search. Used if the artist search failed."""
@@ -41,7 +62,7 @@ def get_new_releases():
     print(f"## Hello, {user.name}! ##\n")
 
     now = datetime.now()
-    print(f"Fetching releases from last {nb_days_delta} days...")
+    print(f"------\tFetching releases from last {nb_days} days...")
 
     for artist_request in user.get_artists(limit=100):
         # artist search
@@ -57,88 +78,90 @@ def get_new_releases():
         for album in artist_result.get_albums():
             release_date = datetime.strptime(album.release_date, '%Y-%m-%d')
             delta = now - release_date
-            delta_limit = timedelta(days=nb_days_delta)
-            if delta < delta_limit:
-                new_releases.append(album)
+            if delta < nb_days_delta:
+                new_releases.append(make_dict(client, album))
         time.sleep(.1)  # to avoid 'API request return error'
 
     nb_releases = len(new_releases)
     if nb_releases == 0:
-        print("No new release found :(")
+        print("-info-\tNo release found :(")
     elif nb_releases == 1:
-        print("A new release found!")
+        print(f"-info-\tOne release found in last {nb_days} days!")
     else:
-        print(f"{nb_releases} new releases found!")
+        print(f"-info-\t{nb_releases} releases found in last {nb_days} days!")
     return new_releases
 
 
 ## NOTION.SO ##
 
-def find_releases_section(page):
-    """Find the 'New releases' section on page."""
-    in_releases_section = False
-    start_id = None
-    already_in_notion = []
-    for child in page.children:
-        if hasattr(child, 'title') and 'New releases' in child.title:
-            in_releases_section = True
-            start_id = child.id
-        elif in_releases_section and type(child) == TodoBlock:
-            already_in_notion.append(child.title)
-    if start_id is None:
-        print("Cannot find the 'New releases' section in this page!")
-        raise NewReleasesSectionNotFound()
-
-    return start_id, already_in_notion
-
-def remove_ticked_elements(page, start_id):
-    """Browse blocks and remove the checked todo [] blocks if they are in the
-    'New releases' section."""
-    in_releases_section = False
+def remove_old_checked_elements(cv):
+    """"""
+    now = datetime.now()
     nb_cleaned = 0
 
-    for child in page.children:
-        if child.id == start_id:
-            in_releases_section = True
-            continue
-
-        elif in_releases_section and type(child) is TodoBlock:
-            if child.checked:  # already listened
-                child.remove(permanently=True)
+    for row in cv.collection.get_rows():
+        if row.done:
+            delta = now - (row.added + utc_offset_delta)
+            if remove_all_checked or delta > nb_days_delta - timedelta(hours=12):  # to be sure there is no overlapping
+                row.remove()
                 nb_cleaned += 1
-        elif in_releases_section and type(child) is ImageBlock:
-            child.remove(permanently=True)
 
     if nb_cleaned >= 1:
-        print(f"Cleaned 1 checked element." if nb_cleaned == 1 else f"Cleaned {nb_cleaned} checked elements.")
+        print(f"-info-\tCleaned 1 checked element." if nb_cleaned == 1 else f"-info-\tCleaned {nb_cleaned} checked elements.")
+
+def already_in_database(cv, album_dict):
+    for row in cv.collection.get_rows():
+        if row.url == album_dict["url"]:
+            return True
+    return False
 
 def add_to_notion(new_releases):
-    """Add new releases to the Notion.so page at the right place."""
+    """Add new releases to the Notion.so page in the right collection."""
+    print(f"------\tAdding to Notion...")
     client = NotionClient(token_v2=TOKEN_V2)
-    page = client.get_block(PAGE_URL)
+    cv = client.get_collection_view(COLLECTION_URL)
 
-    start_id, already_in_notion = find_releases_section(page)
-    start_block = client.get_block(start_id)
+    nb_releases_to_add = len(new_releases)
 
-    # add new titles
-    for album in new_releases:
-        block_title = format_release(album)
-        if block_title not in already_in_notion:
-            release_block = page.children.add_new(block_type=TodoBlock, title=block_title)
-            # todo: move this block to before the next section
-    if len(new_releases) >= 1:
-        print("Added albums to Notion.so!")
-    # remove ticked elements
-    remove_ticked_elements(page, start_id)
+    for album_dict in new_releases:  # for each new release
 
-    # add a random photo among new albums at the bottom of the page
-    if len(new_releases) >= 1:  # at least one new album
-        photo_block = page.children.add_new(block_type=ImageBlock, width=300)
-        photo_block.set_source_url(album.cover_big)
-        print("... and a little cover :)")
+        # test if already in database using the url
+        if already_in_database(cv, album_dict):
+            nb_releases_to_add -= 1
+            continue
+
+        # if new
+        row = cv.collection.add_row()
+        for key, value in album_dict.items():
+            if key == "genre":
+                try:
+                    if value is None:  # i.e. if no genre on Deezer
+                        raise NoGenreFound()
+                    else:
+                        setattr(row, key, value)
+                except NoGenreFound:
+                    pass
+                except ValueError:  # i.e. if genre does not exist yet in Notion
+                    print(f"-warn-\tPlease add genre {value} to database!")
+            else:
+                setattr(row, key, value)
+
+    if nb_releases_to_add == 0:
+        print("-info-\tAlbums already added.")
+    elif nb_releases_to_add == 1:
+        print("-info-\tAdded 1 album to Notion.so!")
+    else:
+        print(f"-info-\tAdded {nb_releases_to_add} albums to Notion.so!")
+
+    # remove checked elements of last week
+    remove_old_checked_elements(cv)
+    print()
+    print("-info-\tDone!")
 
 
-class NewReleasesSectionNotFound(Exception):
+## EXCEPTIONS
+
+class NoGenreFound(Exception):
     pass
 
 
